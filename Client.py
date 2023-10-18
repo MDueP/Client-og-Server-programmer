35c3997085958fc6d3e92f70dbe8fa0a203ff99d
MDueP <mduepedersen@outlook.dk>
2023-10-18 15:24:44 +0200

    Færdig

---
+++ Client.py
@@ -0,0 +1,16 @@
+from socket import *
+
+serverName = '172.16.0.40'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+print()
+print("==========================")
+print("Klar til at sende beskeder")
+print("==========================")
+print()
+print()
+
+while True:
+    message=input('Skriv noget til serveren: ')
+    clientSocket.sendto(message.encode(),(serverName, serverPort))
